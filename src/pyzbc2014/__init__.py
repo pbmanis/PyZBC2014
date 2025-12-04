e89809aebace699d1d4cd55d623bc8b1aa7a50c9
pbmanis <pmanis@med.unc.edu>
2025-12-04 12:13:34 -0500

    fix to work with class version

--- src/pyzbc2014/__init__.py
+++ src/pyzbc2014/__init__.py
@@ -1,2 +1,3 @@
-from .pyzbc2014 import sim_ihc_zbc2014, sim_anrate_zbc2014
-__version__ = "0.0.2"
+# from .pyzbc2014 import sim_ihc_zbc2014, sim_anrate_zbc2014, sim_spike_generator_zbc2014
+import pyzbc2014
+__version__ = "0.0.3"
