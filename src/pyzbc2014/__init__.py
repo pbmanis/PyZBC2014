19fcbe1b2b0e05ec2684722edf9edba7c8ab71bd
Paul Manis <pmanis@med.unc.edu>
2026-07-20 11:06:41 -0400

    change import for module

--- src/pyzbc2014/__init__.py
+++ src/pyzbc2014/__init__.py
@@ -1,3 +1,5 @@
-# from .pyzbc2014 import sim_ihc_zbc2014, sim_anrate_zbc2014, sim_spike_generator_zbc2014
-import pyzbc2014
+from .pyzbc2014 import pyzbc2014
+
 __version__ = "0.0.3"
+
+__all__ = ["pyzbc2014"]
